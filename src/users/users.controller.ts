210bfbd753c48af0fd0233a34cdb3e68f3cd0fe0
Tedros Kahsu <tedkahcom@gmail.com>
2024-12-17 13:56:15 +0300

    Add users module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(
+    @Body() data: { username: string; email: string; password: string },
+  ) {
+    return this.usersService.register(data);
+  }
+
+  @Get()
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(data: { username: string; email: string; password: string }) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (existingUser) {
+      throw new Error('User with this email already exists');
+    }
+
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+}
