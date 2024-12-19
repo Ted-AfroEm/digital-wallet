a4b39e2c67b52c2904ac6228c6fe4c4260d7a092
Tedros Kahsu <tedkahcom@gmail.com>
2024-12-19 04:36:04 +0300

    Remove auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+// import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransactionsModule } from './transactions/transactions.module';
-import { AuthModule } from './auth/auth.module';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+// import { AuthModule } from './auth/auth.module';
+// import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
-  imports: [UsersModule, AccountsModule, TransactionsModule, AuthModule],
+  imports: [UsersModule, AccountsModule, TransactionsModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_GUARD, // Apply the JwtAuthGuard globally
-      useClass: JwtAuthGuard,
-    },
+    // {
+    //   provide: APP_GUARD, // Apply the JwtAuthGuard globally
+    //   useClass: JwtAuthGuard,
+    // },
   ],
 })
 export class AppModule {}
