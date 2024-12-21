4614ecf744fa9981b23ed3b5a071f11f5e09e3a2
Tedros Kahsu <tedkahcom@gmail.com>
2024-12-21 04:29:05 +0300

    Test Transaction service

--- backend/src/transactions/transactions.service.spec.ts
+++ backend/src/transactions/transactions.service.spec.ts
@@ -1,18 +1,37 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { TransactionsService } from './transactions.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TransactionsService } from './transactions.service';
+import { PrismaService } from '../common/prisma.service';
+import { BadRequestException } from '@nestjs/common';
 
-// describe('TransactionsService', () => {
-//   let service: TransactionsService;
+describe('TransactionsService', () => {
+  let service: TransactionsService;
+  let prisma: PrismaService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [TransactionsService],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TransactionsService,
+        {
+          provide: PrismaService,
+          useValue: {
+            account: {
+              findUnique: jest.fn(),
+              update: jest.fn(),
+            },
+            transaction: {
+              create: jest.fn(),
+            },
+            $transaction: jest.fn((callback) => callback(prisma)),
+          },
+        },
+      ],
+    }).compile();
 
-//     service = module.get<TransactionsService>(TransactionsService);
-//   });
+    service = module.get<TransactionsService>(TransactionsService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
