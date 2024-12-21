1049e2ce8cb82e1a3b3eae26a9dee2be94d94b66
Tedros Kahsu <tedkahcom@gmail.com>
2024-12-21 05:49:12 +0300

    Add account service test

--- backend/src/accounts/accounts.service.spec.ts
+++ backend/src/accounts/accounts.service.spec.ts
@@ -1,18 +1,31 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AccountsService } from './accounts.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AccountsService } from './accounts.service';
+import { PrismaService } from '../common/prisma.service';
 
-// describe('AccountsService', () => {
-//   let service: AccountsService;
+describe('AccountsService', () => {
+  let service: AccountsService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AccountsService],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AccountsService,
+        {
+          provide: PrismaService,
+          useValue: {
+            account: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+            },
+          },
+        },
+      ],
+    }).compile();
 
-//     service = module.get<AccountsService>(AccountsService);
-//   });
+    service = module.get<AccountsService>(AccountsService);
+  });
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
