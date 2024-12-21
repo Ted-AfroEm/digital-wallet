e3cbe8fcda7181077afd93fa8d59690346565a8c
Tedros Kahsu <tedkahcom@gmail.com>
2024-12-21 05:50:05 +0300

    Add account controller test

--- backend/src/accounts/accounts.controller.spec.ts
+++ backend/src/accounts/accounts.controller.spec.ts
@@ -1,18 +1,30 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AccountsController } from './accounts.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AccountsController } from './accounts.controller';
+import { AccountsService } from './accounts.service';
 
-// describe('AccountsController', () => {
-//   let controller: AccountsController;
+describe('AccountsController', () => {
+  let controller: AccountsController;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [AccountsController],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AccountsController],
+      providers: [
+        {
+          provide: AccountsService,
+          useValue: {
+            create: jest.fn(),
+            findAllByUserId: jest.fn(),
+            findAllAccounts: jest.fn(),
+            findOneByUserId: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
 
-//     controller = module.get<AccountsController>(AccountsController);
-//   });
+    controller = module.get<AccountsController>(AccountsController);
+  });
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
