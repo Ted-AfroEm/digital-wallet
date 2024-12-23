9d42cbd899b57a5c9d99d9b05458602a101e1c79
Tedros Kahsu <tedkahcom@gmail.com>
2024-12-23 04:39:07 +0300

    Fix login reload issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
-import { useAuth } from "./context/AuthContext";
 
 const App: React.FC = () => {
-  const { user } = useAuth();
   return (
     <>
       <ToastContainer />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/dashboard"
-          element={user ? <Dashboard /> : <Navigate to="/" />}
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
